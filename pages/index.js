e664c6a290d0cd1ff266bccf8939f35785adfa61
Oussamaosman02 <109099115+Oussamaosman02@users.noreply.github.com>
2022-10-25 12:41:44 +0200

    Adding al the websites and dark mode

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <h1>30/11</h1>
       <main className={styles.main}>
-        <h1>30/11</h1>
         <div className={styles.cont}>
-          <h2>Sites</h2>
+          <h2>Muro de sitios</h2>
           <div>
             <a
               target="_BLANK"
@@ -23,7 +23,7 @@ export default function Home() {
               3011
             </a>
             <hr />
-            <h3>Muro de proyectos</h3>
+            <h3>Muro de webs hechas por mi</h3>
           </div>
           <div>
             <a
@@ -53,7 +53,7 @@ export default function Home() {
               rel="noreferrer noopener"
               href="https://creal.store"
             >
-              CReal Store
+              CReal Store (CReal)
             </a>
             <hr />
             <h3>portal de tiendas de Ciudad real</h3>
@@ -64,10 +64,153 @@ export default function Home() {
               rel="noreferrer noopener"
               href="https://tonias.creal.store"
             >
-              Tonias
+              Tonias (CReal)
             </a>
             <hr />
             <h3>Tienda de ropa administrada en CReal Store</h3>
+          </div>
+          <div>
+            <a
+              target="_BLANK"
+              rel="noreferrer noopener"
+              href="https://notitia.cat"
+            >
+              Notitia
+            </a>
+            <hr />
+            <h3>Web de información e investigación propia</h3>
+          </div>
+          <div>
+            <a
+              target="_BLANK"
+              rel="noreferrer noopener"
+              href="https://genio.me"
+            >
+              Genio Me
+            </a>
+            <hr />
+            <h3>Recursos para desarroladores</h3>
+          </div>
+          <div>
+            <a
+              target="_BLANK"
+              rel="noreferrer noopener"
+              href="https://www.mejoressmartphones.online"
+            >
+              Mejores Smartphones
+            </a>
+            <hr />
+            <h3>Mejores Smartphones por categorías</h3>
+          </div>
+          <div>
+            <a
+              target="_BLANK"
+              rel="noreferrer noopener"
+              href="https://podcas.website"
+            >
+              Podcas
+            </a>
+            <hr />
+            <h3>Todo sobre podcasts</h3>
+          </div>
+          <div>
+            <a
+              target="_BLANK"
+              rel="noreferrer noopener"
+              href="https://www.miseo.cf"
+            >
+              MiSeo
+            </a>
+            <hr />
+            <h3>SEO estrategias y trucos</h3>
+          </div>
+          <div>
+            <a
+              target="_BLANK"
+              rel="noreferrer noopener"
+              href="https://www.creadordigital0.com"
+            >
+              Creador Digital 0
+            </a>
+            <hr />
+            <h3>Web para subir creaciones digitales</h3>
+          </div>
+          <div>
+            <a
+              target="_BLANK"
+              rel="noreferrer noopener"
+              href="https://voteu.tk"
+            >
+              Vote Ú
+            </a>
+            <hr />
+            <h3>Web para votar cosas</h3>
+          </div>
+          <div>
+            <a
+              target="_BLANK"
+              rel="noreferrer noopener"
+              href="https://homosapiens.fun"
+            >
+              Homosapiens Fun
+            </a>
+            <hr />
+            <h3>Web de la plantilla del Barça</h3>
+          </div>
+          <div>
+            <a
+              target="_BLANK"
+              rel="noreferrer noopener"
+              href="https://www.ijotaka.es"
+            >
+              IJotaKa
+            </a>
+            <hr />
+            <h3>Web de libros que he leido o quiero leer</h3>
+          </div>
+          <div>
+            <a
+              target="_BLANK"
+              rel="noreferrer noopener"
+              href="https://homosapiens.site"
+            >
+              Nostradamus
+            </a>
+            <hr />
+            <h3>Predicciones de Nostradamus año a año</h3>
+          </div>
+          <div>
+            <a
+              target="_BLANK"
+              rel="noreferrer noopener"
+              href="https://tiktokbreach.netlify.app"
+            >
+              TikTok Breach
+            </a>
+            <hr />
+            <h3>Información sobre la falsa filtración de TikTok</h3>
+          </div>
+          <div>
+            <a
+              target="_BLANK"
+              rel="noreferrer noopener"
+              href="https://nextjs-push.vercel.app"
+            >
+              Notificaciones NextJs
+            </a>
+            <hr />
+            <h3>prueba de notificaciones push en Next Js</h3>
+          </div>
+          <div>
+            <a
+              target="_BLANK"
+              rel="noreferrer noopener"
+              href="https://velta.es"
+            >
+              Juego 3D
+            </a>
+            <hr />
+            <h3>Landing Page de un juego 3D</h3>
           </div>
         </div>
       </main>
