e1774e21faf1213d25634acffdf8046ef53ec7d0
Oussamaosman02 <109099115+Oussamaosman02@users.noreply.github.com>
2022-11-15 10:36:17 +0100

    Consuming the api

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,239 +17,29 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.cont}>
           <h2>Muro de sitios</h2>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://3011.site"
-            >
-              3011
-            </a>
-            <hr />
-            <h3>Muro de webs hechas por mi</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://creal.es"
-            >
-              CReal
-            </a>
-            <hr />
-            <h3>Conectar a los jovenes de Ciudad real</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://tonyashoes.creal.store"
-            >
-              Tonyashoes (CReal)
-            </a>
-            <hr />
-            <h3>Tienda de zapatos personalizables (3D)</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://creal.store"
-            >
-              CReal Store (CReal)
-            </a>
-            <hr />
-            <h3>portal de tiendas de Ciudad real</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://tonias.creal.store"
-            >
-              Tonias (CReal)
-            </a>
-            <hr />
-            <h3>Tienda de ropa administrada en CReal Store</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://notitia.cat"
-            >
-              Notitia
-            </a>
-            <hr />
-            <h3>Web de información e investigación propia</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://asier.creal.es"
-            >
-              Asier
-            </a>
-            <hr />
-            <h3>PWA para manejar una clase o grupo de personas</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://genio.me"
-            >
-              Genio Me
-            </a>
-            <hr />
-            <h3>Recursos para desarroladores</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://www.mejoressmartphones.online"
-            >
-              Mejores Smartphones
-            </a>
-            <hr />
-            <h3>Mejores Smartphones por categorías</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://podcas.website"
-            >
-              Podcas
-            </a>
-            <hr />
-            <h3>Todo sobre podcasts</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://www.miseo.cf"
-            >
-              MiSeo
-            </a>
-            <hr />
-            <h3>SEO estrategias y trucos</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://js.3011.site"
-            >
-              Terminal JavaScript
-            </a>
-            <hr />
-            <h3>Terminal web para usar JavaScript</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://www.creadordigital0.com"
-            >
-              Creador Digital 0
-            </a>
-            <hr />
-            <h3>Web para subir creaciones digitales</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://voteu.tk"
-            >
-              Vote Ú
-            </a>
-            <hr />
-            <h3>Web para votar cosas</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://homosapiens.fun"
-            >
-              Homosapiens Fun
-            </a>
-            <hr />
-            <h3>Web de la plantilla del Barça</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://ijotaka.com"
-            >
-              IJotaKa COM
-            </a>
-            <hr />
-            <h3>Web de libros que he leido o quiero leer</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://ijotaka.es"
-            >
-              IJotaKa ES
-            </a>
-            <hr />
-            <h3>Web de Agencia digital de prueba</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://homosapiens.site"
-            >
-              Nostradamus
-            </a>
-            <hr />
-            <h3>Predicciones de Nostradamus año a año</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://tiktokbreach.netlify.app"
-            >
-              TikTok Breach
-            </a>
-            <hr />
-            <h3>Información sobre la falsa filtración de TikTok</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://nextjs-push.vercel.app"
-            >
-              Notificaciones NextJs
-            </a>
-            <hr />
-            <h3>prueba de notificaciones push en Next Js</h3>
-          </div>
-          <div>
-            <a
-              target="_BLANK"
-              rel="noreferrer noopener"
-              href="https://velta.es"
-            >
-              Juego 3D
-            </a>
-            <hr />
-            <h3>Landing Page de un juego 3D</h3>
-          </div>
+          {data.map((dat) => {
+            return (
+              <div key={dat.link}>
+                <a target="_BLANK" rel="noreferrer noopener" href={dat.link}>
+                  {dat.name}
+                </a>
+                <hr />
+                <h3>{dat.desc}</h3>
+              </div>
+            );
+          })}
         </div>
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("https://3011.site/api/sites");
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
+  };
 }
