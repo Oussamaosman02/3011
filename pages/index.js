4810f7a89d64a42256987084e128822e99c9242f
oussama02 <oussamaosman02@gmail.com>
2022-11-18 15:34:58 +0100

    Adding pages and standard linter

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
-export default function Home({ data }) {
+export default function Home ({ data }) {
   return (
     <div className={styles.container}>
       <Head>
         <title>30/11</title>
         <meta
-          name="description"
-          content="El muro de webs de un nacido el 30 del 11"
+          name='description'
+          content='El muro de webs de un nacido el 30 del 11'
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <h1>30/11</h1>
@@ -20,26 +20,26 @@ export default function Home({ data }) {
           {data.map((dat) => {
             return (
               <div key={dat.link}>
-                <a target="_BLANK" rel="noreferrer noopener" href={dat.link}>
+                <a target='_BLANK' rel='noreferrer noopener' href={dat.link}>
                   {dat.name}
                 </a>
                 <hr />
                 <h3>{dat.desc}</h3>
               </div>
-            );
+            )
           })}
         </div>
       </main>
     </div>
-  );
+  )
 }
 
-export async function getServerSideProps() {
-  const res = await fetch("https://3011.site/api/sites");
-  const data = await res.json();
+export async function getServerSideProps () {
+  const res = await fetch('https://3011.site/api/sites')
+  const data = await res.json()
   return {
     props: {
-      data,
-    },
-  };
+      data
+    }
+  }
 }
